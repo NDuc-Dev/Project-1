449ad7c5e83d917deea350d98b9140161ecd0c24
NDuc <nguyenngocduc260504@gmail.com>
2023-07-19 14:53:08 +0700

    update 19/7

--- CFMLib/BL/OrderBL.cs
+++ CFMLib/BL/OrderBL.cs
@@ -5,10 +5,10 @@ namespace BL
 {
     public class OrderBL
     {
-        private OrderDAL odl = new OrderDAL();
-        public bool CreateOrder(Order order)
+        private OrderDAL orderDAL = new OrderDAL();
+        public bool SaveOrder(Order order)
         {
-            bool result = odl.CreateOrder(order);
+            bool result = orderDAL.CreateOrder(order);
             return result;
         }
     }
