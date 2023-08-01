bd2b90bea35659783c2eafbac647d2f297bd656a
NDuc <nguyenngocduc260504@gmail.com>
2023-08-01 16:29:16 +0700

    update DAL

--- CFMLib/DAL/OrderDAL.cs
+++ CFMLib/DAL/OrderDAL.cs
@@ -17,66 +17,80 @@ namespace DAL
             {
                 using (MySqlTransaction trans = connection.BeginTransaction())
                 using (MySqlCommand cmd = connection.CreateCommand())
-                
-                {
-                    cmd.Connection = connection;
-                    cmd.Transaction = trans;
-                    cmd.CommandText = "lock tables Orders write, staffs write, product_sizes write, Order_Details write;";
-                    cmd.ExecuteNonQuery();
-
-                    MySqlDataReader? reader = null;
-                    
-
-                    //insert order
-                    cmd.CommandText = "insert into Orders(order_staff_id, order_status) values (@staffId, @orderStatus);";
-                    cmd.Parameters.Clear();
-                    cmd.Parameters.AddWithValue("@staffId", order.OrderStaffID);
-                    cmd.Parameters.AddWithValue("@orderStatus", OrderStatus.ORDER_INPROGRESS);
-                    cmd.ExecuteNonQuery();
-
-                    //get new Order_ID
-                    cmd.CommandText = "select LAST_INSERT_ID() as order_id";
-                    reader = cmd.ExecuteReader();
-                    if (reader.Read())
+                    try
                     {
-                        order.OrderId = reader.GetInt32("order_id");
-                    }
-                    reader.Close();
+                        cmd.Connection = connection;
+                        cmd.Transaction = trans;
+                        cmd.CommandText = "lock tables Orders write, staffs write, product_sizes write, Order_Details write;";
+                        cmd.ExecuteNonQuery();
 
-                    //insert Order Details table
-                    foreach (var item in order.ProductsList)
-                    {
-                        if (item.ProductId == 0 || item.ProductQuantity <= 0)
-                        {
-                            throw new Exception("Not Exists Product");
-                        }
-                        //get unit_price
-                        cmd.CommandText = "select price from product_sizes where product_id=@productId and size_id=@sizeId";
+                        MySqlDataReader? reader = null;
+
+
+                        //insert order
+                        cmd.CommandText = "insert into Orders(order_staff_id, order_status) values (@staffId, @orderStatus);";
                         cmd.Parameters.Clear();
-                        cmd.Parameters.AddWithValue("@productId", item.ProductId);
-                        cmd.Parameters.AddWithValue("@sizeId", item.ProductSizeId);
+                        cmd.Parameters.AddWithValue("@staffId", order.OrderStaffID);
+                        cmd.Parameters.AddWithValue("@orderStatus", OrderStatus.ORDER_INPROGRESS);
+                        cmd.ExecuteNonQuery();
+
+                        //get new Order_ID
+                        cmd.CommandText = "select LAST_INSERT_ID() as order_id";
                         reader = cmd.ExecuteReader();
-                        if (!reader.Read())
+                        if (reader.Read())
                         {
-                            throw new Exception("Not Exists Item");
+                            order.OrderId = reader.GetInt32("order_id");
                         }
-                        item.ProductPrice = reader.GetDecimal("price");
                         reader.Close();
 
-                        //insert to Order Details
-                        cmd.CommandText = @"insert into Order_Details(order_id, product_id, size_id, quantity, amount) values 
-                            (" + order.OrderId + ", " + item.ProductId + ", "+ item.ProductSizeId+ ", " + item.ProductQuantity + ","+ (item.ProductQuantity*item.ProductPrice)+ ");";
+                        //insert Order Details table
+                        foreach (var item in order.ProductsList)
+                        {
+                            if (item.ProductId == 0 || item.ProductQuantity <= 0)
+                            {
+                                throw new Exception("Not Exists Product");
+                            }
+                            //get unit_price
+                            cmd.CommandText = "select price from product_sizes where product_id=@productId and size_id=@sizeId";
+                            cmd.Parameters.Clear();
+                            cmd.Parameters.AddWithValue("@productId", item.ProductId);
+                            cmd.Parameters.AddWithValue("@sizeId", item.ProductSizeId);
+                            reader = cmd.ExecuteReader();
+                            if (!reader.Read())
+                            {
+                                throw new Exception("Not Exists Item");
+                            }
+                            item.ProductPrice = reader.GetDecimal("price");
+                            reader.Close();
+
+                            //insert to Order Details
+                            cmd.CommandText = @"insert into Order_Details(order_id, product_id, size_id, quantity, amount) values 
+                            (" + order.OrderId + ", " + item.ProductId + ", " + item.ProductSizeId + ", " + item.ProductQuantity + "," + (item.ProductQuantity * item.ProductPrice) + ");";
+                            cmd.ExecuteNonQuery();
+                        }
+                        //commit transaction
+                        trans.Commit();
+                        result = true;
+                        trans.Rollback();
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            trans.Rollback();
+                        }
+                        catch { }
+                    }
+                    finally
+                    {
+                        //unlock all tables;
+                        cmd.CommandText = "unlock tables;";
                         cmd.ExecuteNonQuery();
                     }
-                    //commit transaction
-                    trans.Commit();
-                    result = true;
-                    trans.Rollback();
-                }
             }
             catch (Exception ex)
             {
-                // Console.WriteLine($"ERROR: {ex.Message}");
+                Console.WriteLine($"ERROR: {ex.Message}");
             }
             return result;
         }
